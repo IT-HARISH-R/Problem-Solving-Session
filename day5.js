3ec9846c99ce3644d988e94ea8de2f5ef4890d3a
IT-HARISH-R <harishmass27.8@gmail.com>
2025-03-07 10:20:36 +0530

    dayu 6

--- day5.js
+++ day5.js
@@ -3,7 +3,7 @@
 function linearSearch(arr = [], target) {
     console.log(...arr)
     for (let ind = 0; ind < arr.length; ind++) {
-        if(arr[ind]===target){
+        if (arr[ind] === target) {
             return ind
         }
     }
@@ -11,3 +11,26 @@ function linearSearch(arr = [], target) {
 }
 
 console.log(linearSearch([8, 3, 4, 5, 6, 9, 2], 5))
+
+function binarySearch(arr = [], target = 0) {
+
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+        if (arr[mid] === target) {
+            return mid;
+        }
+        else if (arr[mid] > target) {
+            end = mid - 1;
+        }
+        else {
+            start = mid + 1;
+        }
+
+    }
+
+    return -1;
+}
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8], 5))
